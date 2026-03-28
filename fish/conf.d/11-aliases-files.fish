e76daa94b50d4dfe3fc319bfdaabf56f3de59f1d
Nicolas Casatti <ncasatti@gmail.com>
2026-03-28 14:32:08 -0300

    refactor(fish): migrate exa aliases to eza

--- fish/conf.d/11-aliases-files.fish
+++ fish/conf.d/11-aliases-files.fish
@@ -3,22 +3,14 @@
 # ====================
 # Enhanced file management commands
 
-# Better ls - prefer exa, fallback to eza
-if command -q exa
-    # alias ls='exa --git --icons --color=always --group-directories-first -1'
-    alias l='exa -T -L 1 --icons --color=always --group-directories-first'
-    alias la='exa -T -L 1 --icons --color=always --group-directories-first --absolute --hyperlink'
-    alias ll='exa -l --icons --color=always --group-directories-first'
-    alias ls='exa -lah --icons --color=always --group-directories-first'
-    alias l1='exa -T -L 2 --icons --color=always --group-directories-first'
-    alias l2='exa -T -L 3 --icons --color=always --group-directories-first'
-else if command -q eza
-    alias ls='eza -al --git --icons --color=always --group-directories-first'
-    alias la='eza -a --color=always --group-directories-first'
-    alias ll='eza -l --color=always --group-directories-first'
-    alias l='eza -lah --color=always --group-directories-first'
-    alias l1='eza -T -L 1 --color=always --group-directories-first'
-    alias l2='eza -T -L 2 --color=always --group-directories-first'
+# Better ls - use eza
+if command -q eza
+    alias l='eza -T -L 1 --icons --color=always --group-directories-first'
+    alias la='eza -T -L 1 --icons --color=always --group-directories-first --absolute --hyperlink'
+    alias ll='eza -l --icons --color=always --group-directories-first'
+    alias ls='eza -lah --icons --color=always --group-directories-first'
+    alias l1='eza -T -L 2 --icons --color=always --group-directories-first'
+    alias l2='eza -T -L 3 --icons --color=always --group-directories-first'
 end
 
 # File management
