2f31030c876b018553fdf5d5a4f11c7db8e08794
Nicolas Casatti <ncasatti@gmail.com>
2026-04-02 14:00:55 -0300

    fix(nvim): change treesiter to branch master

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,13 @@
 -- Add Mason bin directory to PATH
 vim.env.PATH = vim.fn.stdpath("data") .. "/mason/bin:" .. vim.env.PATH
+vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/site")
 
 require("config.keys")
 require("config.config")
 require("config.lazy")
 
 -- Cargar conexiones de bases de datos locales (si existen)
-local ok, local_dbs = pcall(require, 'local.databases')
+local ok, local_dbs = pcall(require, "local.databases")
 if ok then
-  vim.g.dbs = local_dbs
+	vim.g.dbs = local_dbs
 end
-
