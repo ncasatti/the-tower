5635f6b292b5d4d7ff7094d3cfb3a73d2ac0d01f
Nicolas Casatti <ncasatti@gmail.com>
2026-04-03 23:41:07 -0300

    fix(treesitter): implement conditional config for Nix/Arch compatibility

--- nvim/lua/plugins/editor/treesitter.lua
+++ nvim/lua/plugins/editor/treesitter.lua
@@ -4,7 +4,16 @@ return {
 	build = ":TSUpdate",
 	lazy = false,
 	config = function()
-		require("nvim-treesitter").setup({
+		-- Detect if running in Nix environment
+		local is_nix = string.find(vim.v.progpath, "/nix/store") ~= nil
+
+		-- Define ensure_installed based on environment
+		local ensure_installed
+		if is_nix then
+			-- In Nix, treesitter parsers are provided via nvim-treesitter.withPlugins
+			ensure_installed = {}
+		else
+			-- In Arch (non-Nix), auto-install parsers
 			ensure_installed = {
 				"vim",
 				"lua",
@@ -26,8 +35,13 @@ return {
 				"markdown_inline",
 				"latex",
 				"sql",
-			},
+			}
+		end
+
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = ensure_installed,
 			sync_install = false,
+			auto_install = not is_nix,
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
