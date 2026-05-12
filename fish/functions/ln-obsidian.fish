a8adbc06fd585c7725601e3a6ad8db22becf1b8a
Nicolas Casatti <ncasatti@gmail.com>
2026-05-11 21:03:25 -0300

    feat(fish): add dest name in ln-obsidian

--- fish/functions/ln-obsidian.fish
+++ fish/functions/ln-obsidian.fish
@@ -1,6 +1,6 @@
 function ln-obsidian --description 'Link file to Zettelkasten References'
     if test -z "$argv[1]"
-        echo "Usage: ln-obsidian <file>"
+        echo "Usage: ln-obsidian <file> [dest-name]"
         return 1
     end
 
@@ -9,8 +9,12 @@ function ln-obsidian --description 'Link file to Zettelkasten References'
     set extension (string match -r '\.[^.]+$' $filename)
     set basename_no_ext (string replace -r '\.[^.]+$' '' $filename)
 
-    set dest_filename "$basename_no_ext-linked$extension"
-    set dest_path /home/ncasatti/Documents/Zettelkasten/References/docs/$dest_filename
+    if test -n "$argv[2]"
+        set dest_filename "$argv[2]$extension"
+    else
+        set dest_filename "$basename_no_ext-linked$extension"
+    end
+    set dest_path /home/flyn/.the-grid/zettelkasten/References/docs/$dest_filename
 
     ln $source_file $dest_path
     echo "Linked $filename -> Zettelkasten/References/docs/$dest_filename"
