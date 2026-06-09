da41aa1be63530b31c01fcfbc90e2ccd4fda089a
Nicolas Casatti <ncasatti@gmail.com>
2026-06-09 01:54:14 -0300

    config(nvim): update dashboard

--- nvim/lua/plugins/snacks/dashboard.lua
+++ nvim/lua/plugins/snacks/dashboard.lua
@@ -1,28 +1,38 @@
 return {
-  enabled = true,
-  preset = {
-    keys = {
-      { icon = "󰈞", key = "t", desc = "Find File", action = ":lua Snacks.picker.files()" },
-      { icon = "", key = "n", desc = "New File", action = ":enew" },
-      { icon = "󱩽", key = "g", desc = "Find Text", action = ":lua Snacks.picker.grep()" },
-      { icon = "", key = "r", desc = "Recent Files", action = ":lua Snacks.picker.recent({ filter = { cwd = true } })" },
-      { icon = "", key = "e", desc = "Explore Files", action = ":Oil --float" },
-      { icon = "", key = "c", desc = "Config", action = ":lua Snacks.picker.files({ cwd = vim.fn.stdpath('config') })" },
-      { icon = "󰒲", key = "s", desc = "Restore Session", section = "session" },
-      { icon = " ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-      { icon = "", key = "q", desc = "Quit", action = ":qa" },
-    },
-    header = [[
-███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ██╗
-████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗  ██║
-██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔██╗ ██║
-██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╗██║
-██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚████║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝  ╚═══╝]],
-  },
-  sections = {
-    { section = "header" },
-    { section = "keys",   gap = 1, padding = 1 },
-    { section = "startup" },
-  },
+	enabled = true,
+	preset = {
+		keys = {
+			{ icon = "󰈞", key = "t", desc = "Find File", action = ":lua Snacks.picker.files()" },
+			{ icon = "", key = "n", desc = "New File", action = ":enew" },
+			{ icon = "", key = "g", desc = "Git", action = ":lua Snacks.lazygit()" },
+			{
+				icon = "",
+				key = "r",
+				desc = "Recent Files",
+				action = ":lua Snacks.picker.recent({ filter = { cwd = true } })",
+			},
+			{ icon = "", key = "e", desc = "Explore Files", action = ":Oil --float" },
+			-- {
+			-- 	icon = "",
+			-- 	key = "c",
+			-- 	desc = "Config",
+			-- 	action = ":lua Snacks.picker.files({ cwd = vim.fn.stdpath('config') })",
+			-- },
+			{ icon = "󰒲", key = "s", desc = "Restore Session", section = "session" },
+			{ icon = " ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+			{ icon = "", key = "q", desc = "Quit", action = ":qa" },
+		},
+		header = [[
+████████╗██╗  ██╗███████╗     ██████╗ ██████╗ ██╗██████╗
+ ╚══██╔══╝██║  ██║██╔════╝    ██╔════╝ ██╔══██╗██║██╔══██╗
+    ██║   ███████║█████╗      ██║  ███╗██████╔╝██║██║  ██║
+    ██║   ██╔══██║██╔══╝      ██║   ██║██╔══██╗██║██║  ██║
+    ██║   ██║  ██║███████╗    ╚██████╔╝██║  ██║██║██████╔╝
+    ╚═╝   ╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═╝  ╚═╝╚═╝╚═════╝ ]],
+	},
+	sections = {
+		{ section = "header" },
+		{ section = "keys", gap = 1, padding = 1 },
+		{ section = "startup" },
+	},
 }
