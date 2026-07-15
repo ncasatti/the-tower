11794d750f7b1120188ee2895cedf9c07f2455cd
Nicolas Casatti <ncasatti@gmail.com>
2026-07-14 21:47:10 -0300

    update(flake): engram, claude, opencode

--- nix/packages/custom/engram.nix
+++ nix/packages/custom/engram.nix
@@ -2,16 +2,16 @@
 
 buildGoModule rec {
   pname = "engram";
-  version = "0.1.9";
+  version = "1.19.0";
 
   src = fetchFromGitHub {
     owner = "Gentleman-Programming";
     repo = "engram";
     rev = "v${version}";
-    hash = "sha256-klH6OlgMd5X1Y83db2dcoxYqW6LgxCsvWw3pGZWhh7Y=";
+    hash = "sha256-EGcxj3GVLP5080oqaNFtK1FFv5gSDDyWXX7H1Pzf5Qs=";
   };
 
-  vendorHash = "sha256-hR1PS0oQcUMbsRcfd6rk2uqlXGT8wcll0H8qU09aYg0=";
+  vendorHash = "sha256-O+pC4x4DKNUWr7Sx9iZOjK6a64wrQA4/lnjvkNLBX64=";
 
   doCheck = false;
 
